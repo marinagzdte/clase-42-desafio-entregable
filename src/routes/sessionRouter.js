8eea93a9ca4c444a54ec5ac212978866c3825786
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-11-05 16:28:52 -0300

    use sessionController

--- src/routes/sessionRouter.js
+++ src/routes/sessionRouter.js
@@ -1,42 +1,25 @@
 import passport from '../middlewares/passport.js';
 import { Router } from 'express';
 import logger from '../utils/logger.js';
+import checkAuth from "../middlewares/auth.js";
+import { getLogin, postLogin, getLogout, getLoginError, getRegister, postRegister, getRegisterError, get } from "../controllers/sessionController.js";
 
 const sessionRouter = new Router();
 
-sessionRouter.get('/login', logger.logReqInfo, (req, res) => {
-    res.render('login')
-})
+sessionRouter.get('/login', logger.logReqInfo, getLogin)
 
-sessionRouter.post('/login', passport.authenticate('login', { failureRedirect: '/login/error' }), (req, res) => res.redirect('/'));
+sessionRouter.post('/login', passport.authenticate('login', { failureRedirect: '/login/error' }), postLogin);
 
-sessionRouter.get('/logout', logger.logReqInfo, (req, res) => {
-    req.logout({ keepSessionInfo: false }, (err) => res.redirect('/'))
-})
+sessionRouter.get('/logout', logger.logReqInfo, getLogout)
 
-sessionRouter.get('/login/error', logger.logReqInfo, (req, res) => {
-    res.render('autherror', { loginError: true })
-})
+sessionRouter.get('/login/error', logger.logReqInfo, getLoginError)
 
-sessionRouter.get('/register', logger.logReqInfo, (req, res) => {
-    res.render('register');
-})
+sessionRouter.get('/register', logger.logReqInfo, getRegister)
 
-sessionRouter.post('/register', passport.authenticate('register', { failureRedirect: '/register/error' }), (req, res) => { res.redirect('/login') });
+sessionRouter.post('/register', passport.authenticate('register', { failureRedirect: '/register/error' }), postRegister);
 
-sessionRouter.get('/register/error', logger.logReqInfo, (req, res) => {
-    res.render('autherror', { registerError: true })
-})
+sessionRouter.get('/register/error', logger.logReqInfo, getRegisterError)
 
-const checkAuth = (req, res, next) => {
-    if (req.isAuthenticated())
-        next();
-    else
-        res.redirect('/login');
-}
-
-sessionRouter.get('/', checkAuth, (req, res) => {
-    res.render('main', { name: req.user.name, username: req.user.username, avatar: req.user.avatar });
-});
+sessionRouter.get('/', checkAuth, get);
 
 export default sessionRouter;
