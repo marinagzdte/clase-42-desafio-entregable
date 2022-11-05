eff3dc0bc1f9643a374b8b5f8fa42f140c0fa437
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-11-05 15:56:58 -0300

    refactor routers

--- src/routes/sessionRouter.js
+++ src/routes/sessionRouter.js
@@ -22,10 +22,7 @@ sessionRouter.get('/register', logger.logReqInfo, (req, res) => {
     res.render('register');
 })
 
-sessionRouter.post(
-    '/register',
-    passport.authenticate('register', { failureRedirect: '/register/error' }),
-    (req, res) => { res.redirect('/login') });
+sessionRouter.post('/register', passport.authenticate('register', { failureRedirect: '/register/error' }), (req, res) => { res.redirect('/login') });
 
 sessionRouter.get('/register/error', logger.logReqInfo, (req, res) => {
     res.render('autherror', { registerError: true })
